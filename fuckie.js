8802beb1d3cabb145aa167e2f6b67dcf8b27ad6a
Gregor Adams <greg@pixelass.com>
2015-05-02 17:58:14 +0200

    Revert "use forEach"
    This reverts commit d55f924b389c3c83a8ec0e2f674f77bda380009d.

--- fuckie.js
+++ fuckie.js
@@ -1,9 +1,9 @@
 var myNav = navigator.userAgent.toLowerCase();
-var isIE = (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+var isIE = (myNav.indexOf('msie') != -1) || (myNav.indexOf('edge') != -1) ? parseInt(myNav.split('msie')[1]) : false;
 
 if (isIE) {
-    var links = document.querySelectorAll("link");
-    Array.prototype.forEach.call(links, function(link){
-        link.href = '';
-    });
+    var link = document.querySelectorAll("link");
+    for (var i = 0; i < link.length; i++) {
+        link[i].href = '';
+    }
 }
