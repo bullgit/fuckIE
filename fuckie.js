c858b43df9d4d98111b937eb2fd7d1b3bfe20157
Lucas Bonomi <lucas.bonomi@gmail.com>
2015-08-05 16:41:11 +0200

    Update script to target all IE versions & browsers

--- fuckie.js
+++ fuckie.js
@@ -1,6 +1,4 @@
-var myNav = navigator.userAgent.toLowerCase();
-
-if ((myNav.indexOf('msie') !== -1) || (myNav.indexOf('edge') !== -1)) {
+if (window.navigator.msPointerEnabled) {
   var links = document.querySelectorAll("link");
   Array.prototype.forEach.call(links, function (link) {
     link.href = '';
