91014ada5f68298cc90da01e81f2e7c677084b16
0x04 <ok@0x04.de>
2018-07-06 12:06:23 +0200

    refactor: simplified the script to work with IE <= 6
    It's not possible to transform a HTMLCollections into an arrays in IE <= 8, so it now uses a simple for loop.

--- fuckie.js
+++ fuckie.js
@@ -1,50 +1,26 @@
-if (window.msWriteProfilerMark || !!window.ActiveXObject) {
+(function() {
+  if (window.msWriteProfilerMark || !!window.ActiveXObject) {
+    function removeStyles(elements) {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-  var slice = Array.prototype.slice;
-  var links = document.getElementsByTagName('link');
-  var styles = document.getElementsByTagName('style');
-  var walk = function (nodes, cb) {
-    if (!('length' in nodes)) {
-      nodes = [nodes];
-    }
-    nodes = slice.call(nodes);
-    while (nodes.length) {
-      var node = nodes.shift(),
-        ret = cb(node);
-      if (ret) {
-        return ret;
+        switch (element.tagName) {
+          case 'LINK':
+          case 'STYLE':
+            element.parentElement.removeChild(element);
+            break;
+
+          default:
+            if (!!element.style) {
+              element.style.cssText = '';
+            }
+            break;
+        }
       }
-      if (node.childNodes && node.childNodes.length) {
-        nodes = slice.call(node.childNodes).concat(nodes);
-      }
     }
-  };
 
-  Array.prototype.forEach = Array.prototype.forEach || function forEach(callback, thisArg) {
-    if (typeof callback !== 'function') {
-      throw new TypeError(callback + ' is not a function');
-    }
-    var array = this;
-    thisArg = thisArg || this;
-    for (var i = 0, l = array.length; i !== l; ++i) {
-      callback.call(thisArg, array[i], i, array);
-    }
-  };
-
-  Array.prototype.forEach.call(links, function (link) {
-    link.href = '';
-  });
-
-  Array.prototype.forEach.call(styles, function (style) {
-    if (!style) {
-      return;
-    }
-    style.parentElement.removeChild(style);
-  });
-
-  walk(document.body, function (dom) {
-    if (dom.style) {
-      dom.style.cssText = '';
-    }
-  });
-}
+    removeStyles(document.getElementsByTagName('*'));
+    // Note: Due a strange bug `document.getElementsByTagName('*')` doesn't contain `style` tags
+    removeStyles(document.getElementsByTagName('style'));
+  }
+})();
